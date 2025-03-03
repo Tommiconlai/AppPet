e46c126311c94b6326876de8bacdda55a6802a25
Gabriele Tomasso <gabriele.tomasso@itsrizzoli.it>
2025-03-03 11:55:49 +0100

    aggiunto intent da login a registrazione

--- Android/app/src/main/java/com/example/apppet/LoginActivity.java
+++ Android/app/src/main/java/com/example/apppet/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.example.apppet;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +16,16 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        TextView goToRegistration = (TextView) findViewById(R.id.registrazioneTxt);
+        goToRegistration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
         });
+
     }
 }
