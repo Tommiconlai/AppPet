95a2b0627d7c02b833b054709cb9235c80c1ad09
Luca Gabello <luca.gabello@itsrizzoli.it>
2025-03-03 15:09:38 +0100

    grafica cartella clinica e classe LogCC

---
+++ Android/app/src/main/java/com/example/apppet/CustomAdapterCC.java
@@ -0,0 +1,55 @@
+package com.example.apppet;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+import java.util.List;
+
+    public class CustomAdapterCC extends BaseAdapter {
+        private Context context;
+        private List<LogCartellaClinica> lista;
+
+        public CustomAdapterCC(Context context, List<LogCartellaClinica> lista) {
+            this.context = context;
+            this.lista = lista;
+        }
+
+        @Override
+        public int getCount() {
+            return lista.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return lista.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = LayoutInflater.from(context).inflate(R.layout.activity_cartella_clinica, parent, false);
+            }
+
+            TextView txtTitle = convertView.findViewById(R.id.txtTitle);
+            TextView txtDate = convertView.findViewById(R.id.txtDate);
+            TextView txtDescription = convertView.findViewById(R.id.txtDescription);
+
+            LogCartellaClinica currentItem = lista.get(position);
+
+            txtTitle.setText(currentItem.getTitle());
+            txtDate.setText(currentItem.getDate());
+            txtDescription.setText(currentItem.getDescription());
+
+            return convertView;
+        }
+    }
+
+
