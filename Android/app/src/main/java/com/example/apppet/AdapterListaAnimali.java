a789857c899b90c9651dae4040cb163549c56dc1
Gabriele Tomasso <gabriele.tomasso@itsrizzoli.it>
2025-03-03 14:57:26 +0100

    fix dei merge

--- Android/app/src/main/java/com/example/apppet/AdapterListaAnimali.java
+++ Android/app/src/main/java/com/example/apppet/AdapterListaAnimali.java
@@ -12,47 +12,6 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.time.Instant;
 import java.util.ArrayList;
 
-public class AdapterListaAnimali extends RecyclerView.Adapter<AdapterListaAnimali.ViewHolder> {
-    Context context;
-    ArrayList<String> arrayList;
-    OnItemClickListener onItemClickListener;
+public class AdapterListaAnimali {
 
-    public AdapterListaAnimali(Context context, ArrayList<String> arrayList) {
-        this.context = context;
-        this.arrayList = arrayList;
-    }
-
-    @NonNull
-    @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(context).inflate(R.layout.animali_image_item, parent, false);
-        return new ViewHolder(view);
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        Glide.with(context).load(arrayList.get(position)).into(holder.imageView);
-        holder.itemView.setOnClickListener(view -> onItemClickListener.onClick(holder.imageView, arrayList.get(position)));
-    }
-
-    @Override
-    public int getItemCount() {
-        return arrayList.size();
-    }
-
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-        ImageView imageView;
-        public ViewHolder(@NonNull View itemView) {
-            super(itemView);
-            imageView = itemView.findViewById(R.id.list_item_image);
-        }
-    }
-
-    public void setOnItemClickListener(OnItemClickListener onItemClickListener) {
-        this.onItemClickListener = onItemClickListener;
-    }
-
-    public interface OnItemClickListener {
-        void onClick(ImageView imageView, String path);
-    }
 }
--- Android/app/src/main/java/com/example/apppet/LoginActivity.java
+++ Android/app/src/main/java/com/example/apppet/LoginActivity.java
@@ -22,7 +22,7 @@ public class LoginActivity extends AppCompatActivity {
         goToRegistration.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, SecondActivity.class);
+                Intent intent = new Intent(LoginActivity.this, Registrazione.class);
                 startActivity(intent);
             }
         });
