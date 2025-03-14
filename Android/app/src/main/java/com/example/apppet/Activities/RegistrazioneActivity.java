561369b880b6faa23095fe6e1aa066622de0826d
Luca Gabello <luca.gabello@itsrizzoli.it>
2025-03-14 15:41:38 +0100

    registrazione utente

--- Android/app/src/main/java/com/example/apppet/Activities/RegistrazioneActivity.java
+++ Android/app/src/main/java/com/example/apppet/Activities/RegistrazioneActivity.java
@@ -1,6 +1,8 @@
 package com.example.apppet.Activities;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,6 +14,13 @@ public class RegistrazioneActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrazione);
+        EditText nomeUtente=findViewById(R.id.NomeUtenteEditText);
+        EditText passwordUtente=findViewById(R.id.pwUtenteEditText);
+        EditText mailUtente=findViewById(R.id.mailUtenteEditText);
+        EditText cognomeUtente=findViewById(R.id.cognomeUtenteEditText);
+        EditText telefonoUtente=findViewById(R.id.cellulareUtenteEditText);
+        Button registratiBTN = findViewById(R.id.registratiBTN);
+
 
     }
 }
