aa1d17688877f1437908949a8463c3a654324a3b
Luke <luca.albertario@itsrizzoli.it>
2025-03-18 12:19:01 +0100

    Aggiunta Autocomplete_Servizi

--- Android/app/src/main/java/com/example/apppet/Activities/RicercaServiziActivity.java
+++ Android/app/src/main/java/com/example/apppet/Activities/RicercaServiziActivity.java
@@ -2,6 +2,8 @@ package com.example.apppet.Activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,13 +21,25 @@ public class RicercaServiziActivity extends AppCompatActivity implements Recycle
     private ServizioAdapter adapter;
     private ArrayList<Servizio> listaServizi = new ArrayList<>();
 
+    AutoCompleteTextView autoCompleteTextView;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ricerca_servizi);
 
+        //CREO L'AUTOCOMPLETE DEI TIPI DI SERVIZIO E SETTO L'ADAPTER
+        autoCompleteTextView = findViewById(R.id.AutoCompltxt);
+        String [] itemArray = getResources().getStringArray(R.array.ListaServizi);
+        ArrayAdapter <String> arrayAdapter = new ArrayAdapter<String>(RicercaServiziActivity.this, android.R.layout.simple_list_item_1,itemArray);
+        autoCompleteTextView.setAdapter(arrayAdapter);
+        
+
+
+        //USO LA RECYCLERVIEW
         RecyclerView recyclerViewServ = findViewById(R.id.listviewServ);
 
+        //GENERO SERVIZI DI PROVA
         // Creazione dati fittizi
         Servizio s1 = new Servizio(1, "Toilettatura", "Taglio e piega", "HairConditioner", "Via Roma", 10, "9:30");
         Servizio s2 = new Servizio(2, "Addestratore", "Addestramento cane", "CaneWay", "Via Milano", 25, "9:30");
@@ -35,6 +49,8 @@ public class RicercaServiziActivity extends AppCompatActivity implements Recycle
         listaServizi.add(s2);
         listaServizi.add(s3);
 
+
+        //SETTO IL SERVIZIO_ADAPTER E SPECIFICO L'INTERFCCIA
         adapter = new ServizioAdapter(this, listaServizi, this);
         recyclerViewServ.setAdapter(adapter);
         recyclerViewServ.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
@@ -43,6 +59,8 @@ public class RicercaServiziActivity extends AppCompatActivity implements Recycle
     @Override
     public void onItemClicked(int position) {
         Intent intent = new Intent(RicercaServiziActivity.this, ProfiloServizio.class);
+
+        //INTENT DEL PUT EXTRA PER SETTARE L'INSERIMENTO DEI CAMPI
 
         intent.putExtra("NOME", listaServizi.get(position).getNome_attivita());
         intent.putExtra("DESCRIZIONE", listaServizi.get(position).getDescrizione());
