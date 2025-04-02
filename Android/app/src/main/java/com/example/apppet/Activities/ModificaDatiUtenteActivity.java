a9c797e41cdad31361cf521822f9b0fc840b4806
Luke <luca.albertario@itsrizzoli.it>
2025-04-02 18:24:52 +0200

    Aggiunta dati in ModificaDatiUtente_Activity

--- Android/app/src/main/java/com/example/apppet/Activities/ModificaDatiUtenteActivity.java
+++ Android/app/src/main/java/com/example/apppet/Activities/ModificaDatiUtenteActivity.java
@@ -1,5 +1,7 @@
 package com.example.apppet.Activities;
 
+import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,14 +13,24 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.apppet.R;
+import com.example.apppet.utente.Utente;
 
 public class ModificaDatiUtenteActivity extends AppCompatActivity {
 
+    Utente u1 = new Utente("", "", "", "", "");
 
+    SharedPreferences sharedPreferences;
+
+    long idUtente;
+
+    @SuppressLint("SuspiciousIndentation")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modifica_dati_utente);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("user_pref", MODE_PRIVATE);
+        idUtente = sharedPreferences.getLong("userId", 0);
 
 
         Button button = findViewById(R.id.modificaButton);
@@ -35,6 +47,13 @@ public class ModificaDatiUtenteActivity extends AppCompatActivity {
             String cognome = cognomeET.getText().toString();
             String telefono = telefonoET.getText().toString();
             String mail = mailET.getText().toString();
+
+            u1.setNome(nome);
+            u1.setCognome(cognome);
+            u1.setTelefono(telefono);
+            u1.setEmail(mail);
+
+
 
             // logica per inviare i dati modificati al server
             finish();
