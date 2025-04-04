8f444f14e07cb8ec9dcfd292476af5b264c35810
FedericaPanceri <federica.panc84@gmail.com>
2025-04-04 17:53:33 +0200

    aggiunta rating bar profilo animale

--- Android/app/src/main/java/com/example/apppet/Activities/ProfiloAnimaleActivity.java
+++ Android/app/src/main/java/com/example/apppet/Activities/ProfiloAnimaleActivity.java
@@ -6,6 +6,7 @@ import android.os.Parcelable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -48,6 +49,14 @@ public class ProfiloAnimaleActivity extends AppCompatActivity {
         TextView cancellaprofiloAnimale = findViewById(R.id.cancellaBTN);
         ImageButton apriCartellaClinica = findViewById(R.id.cartellaClinicaBTN);
         Button btnBackHome = findViewById(R.id.btnBackHome);
+        RatingBar ratingBar = findViewById(R.id.rating_animale2);
+
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                Toast.makeText(ProfiloAnimaleActivity.this, String.valueOf(rating), Toast.LENGTH_SHORT).show();
+            }
+        });
 
         //Set Text
         tvNomeAnimale.setText(animale.getNome());
